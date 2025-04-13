ac9b3cf97f1687ed799e3d48ce8061148c5bebb3
Nikshep M P <nikhilmpgowda@gmail.com>
2025-04-13 22:43:21 +0530

    Clash of Code Easy Problem

---
+++ NIntegerString_to_Array/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string inputString = Console.ReadLine();
+        int odd = 0, even = 0;
+        int[] numArray = inputString.Split(',').Select(s => int.Parse(s.Trim())).ToArray();
+        foreach (int n in numArray)
+        {
+            if (n % 2 == 0)
+            {
+                even += n;
+            }
+            else
+            {
+                odd += n;
+            }
+        }
+        Console.WriteLine($"{odd} {even}");
+    }
+    Console.ReadLine();
+}
