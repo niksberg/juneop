e91b2a7b17a04330a178a3664532ac884cb3a90f
Nikshep M P <nikhilmpgowda@gmail.com>
2025-04-14 23:52:04 +0530

    Anagram Group - Neet Code Medium

---
+++ GroupAnagram-Efficient/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Solution
+{
+    //List of {String List} would be the best Data Structure to store the grouped anagrams
+    public List<List<string>> GroupAnagrams(string[] strs) // strs contains the given ungrouped anagram list
+    {
+        var res = new Dictionary<string, List<string>>();
+        foreach (var s in strs) // Loop through each string in the input array
+        {
+            int[] count = new int[26]; // Array to count occurrences of each character (assuming lowercase a-z)
+            foreach (char c in s) // Loop through each character in the string
+            {
+                count[c - 'a']++; // Increment the count for all aplhabets from a-z by 65 ASCI Value of 'a'
+            }
+            string key = string.Join(",", count); // Create a unique key based on the character counts
+            if (!res.ContainsKey(key)) // Check if the key already exists in the dictionary
+            {
+                res[key] = new List<string>(); // If not, create a new list for this key
+            }
+            res[key].Add(s); // Add the original {string,s} to the list for this key
+        }
+        return res.Values.ToList<List<string>>(); // Return the grouped anagrams as a list of lists
+    }
+}
+
+public static void Main()
+{
+    Solution s = new Solution();
+    string[] input = { "eat", "tea", "tan", "ate", "nat", "bat" };
+    List<List<string>> result = s.GroupAnagrams(input);
+    foreach (var group in result)
+    {
+        Console.WriteLine(string.Join(", ", group));
+    }
+}
---
+++ GroupAnagram/Program.cs
@@ -0,0 +1,33 @@
+﻿//GROUPING ANAGRAM PROBLEM - Medium Level
+
+public class Solution_withSortion
+{
+    //{List of {String List}} would be the best Data Structure to store the grouped anagrams
+    public List<List<string>> GroupAnagrams1(string[] strs) //strs contains the given ungrouped anagram list
+    {
+        var res1 = new Dictionary<string, List<string>>(); //Dictionary to store the sorted word as key and the list of anagrams containing same words as value
+        foreach (var s in strs)
+        {
+            char[] charArray = s.ToCharArray(); //Converting String to Array to Sorting
+            Array.Sort(charArray);
+            string key = new string(charArray); //Sorted Word is Considered as Unique Key.
+            if (!res.Contains(key))
+            {
+                res[key] = new List<string>(); //Sorted_Word -> List of Words in {strs} Contains the same letters
+            }
+            res[key].Add(s); //Add Original Word in the List
+        }
+        return res.Value.ToList<List<string>>(); //Return the List in a LIST through GroupAnagram
+    }
+}
+
+public static void Main()
+{
+    string[] input = { "eat", "tea", "tan", "ate", "nat", "bat" };
+    Solution_withSortion solution = new Solution_withSortion();
+    List<List<string>> result = solution.GroupAnagrams1(input);
+    foreach (var group in result)
+    {
+        Console.WriteLine(string.Join(", ", group));
+    }
+}
