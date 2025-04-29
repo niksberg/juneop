493240ebf1702c6bb2d0d3bea24433d8cd58637f
Nikshep M P <nikhilmpgowda@gmail.com>
2025-04-29 09:42:23 +0530

    Duplicate Solution Using HashSet

---
+++ Duplicate/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public bool Solution(int[] nums)
+    {
+        HashSet<int> seen = new HashSet<int>();
+        foreach (int num in nums)
+        {
+            if (seen.Contains(num))
+            {
+                return true;
+            }
+            seen.Add(num);
+        }
+        return false;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int[] nums = { 1, 2, 3, 1 };
+        bool result = solution.Solution(nums);
+        Console.WriteLine(result); // Output: True
+    }
+}
